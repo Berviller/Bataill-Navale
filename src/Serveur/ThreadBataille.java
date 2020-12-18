2392392ea06ad67e44f5304e9b6323df27afe6a5
Berviller <71489790+Berviller@users.noreply.github.com>
2020-12-18 19:56:07 +0100

    Fin Thread
    Le programme fonctionne depuis un bon moment finalement. C'est juste que c'est du tour par tour donc 1 joueur fait une action, puis l'autes, puis de nouveau le premier etc...

--- src/Serveur/ThreadBataille.java
+++ src/Serveur/ThreadBataille.java
@@ -158,19 +158,24 @@ public void run() {
 				plateau12.attaquer(c1, lig1);
 				plateau22.attaquer(c1, lig1);
 				break;
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+			case "3" :
+				out1.println("veuillez écrire votre message : ");
+				String messagej1 = in1.readLine();
+				messagej1="Partie "+id+", Joueur 1 : "+messagej1;
+				System.out.println(messagej1);
+				out2.println(messagej1);
+				break;
 		}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		out1.println("Votre plateau :\n");
 		out1.println(plateau11.affichage());
 		
 		out1.println("Plateau adverse :\n");
 		out1.println(plateau12.affichage());
 		
-		
-		String message1=in1.readLine();
-		message1="Partie "+id+", Joueur 1 : "+message1;
-		System.out.println(message1);
-		
-		
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		
 		//information des actions possibles et traitement de ces actions chez le joueur 2
 		out2.println("Veuillez choisir l'action que vous souhaitez réaliser :\n-1 : Placer un bateau\n-2 : Attaquer\n-3 : Envoyer un message");
@@ -243,18 +248,22 @@ public void run() {
 				plateau21.attaquer(c2, lig2);
 				plateau11.attaquer(c2, lig2);
 				break;
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+			case "3" :
+				out2.println("veuillez écrire votre message : ");
+				String messagej2 = in2.readLine();
+				messagej2="Partie "+id+", Joueur 2 : "+messagej2;
+				System.out.println(messagej2);
+				out1.println(messagej2);
+				break;
 		}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		out2.println("Votre plateau :\n");
 		out2.println(plateau11.affichage());
 		
 		out2.println("Plateau adverse :\n");
 		out2.println(plateau12.affichage());
 		
-		
-		out2.println("Veuillez choisir l'action que vous souhaitez réaliser :\n-1 : Placer un bateau\n-2 : Attaquer\n-3 : Envoyer un message");
-		String message2=in2.readLine();
-		message2="Partie "+id+", Joueur 2 : "+message2;
-		System.out.println(message2);
 	}
 	}catch (Exception e) {}
 }
